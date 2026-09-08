2699a0f42b794d44c71fc93aec2bd5e61e8c565c
rentpartnerin <rentpartner.in@gmail.com>
2026-09-07 22:56:37 -0700

    update-link

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 export const APP_CONFIG = {
   brandName: "RentPartner",
   whatsappNumber: "91+9057708005",
-  telegramUrl: "https://t.me/RentPartner_Booking",
+  telegramUrl: "https://t.me/Rent_Partner",
   supportEmail: "rentpartner.in@gmail.com",
   supportPhone: "+91 9057708005",
 };
